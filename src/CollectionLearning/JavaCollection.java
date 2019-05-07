d25056fc2cf732343fad0d9a6f6e67d91b5fc356
cf86cf <837948421@qq.com>
2019-05-07 22:01:59 +0800

    packaged

---
+++ src/CollectionLearning/JavaCollection.java
@@ -0,0 +1,98 @@
+package CollectionLearning;
+
+import java.util.*;
+
+/**
+ * Java集合类
+ * map 和set的使用
+ *
+ * Map的使用 key-value
+ * map直接遍历
+ *
+ * 使用Iterator遍历Collection中的元素
+ * containsKey
+ * containsValue
+ *
+ * HashSet和 LinkedHashSet的区别
+ */
+public class JavaCollection {
+    public static void main(String[] args){
+        //创建集合，添加元素
+        int month[] = {31,28,31,30,31};
+        String strmonth[] = {"Jan","Feb","Mar","Apr","May"};
+        /**
+         * HashSet 和LinkedHashSet 区别，
+         * HashSet 按照 hashcode的值 进行存储数据
+         * LinkedHashSet  保存了记录的插入顺序
+         *
+         *  同理适用于 HashMap
+         */
+
+        Map<String, Integer> map = new LinkedHashMap<>();
+        //Map<String, Integer> map = new HashMap<>();   //使用HashMap 时的输出顺序为升序
+
+        Collection<Month> setint = new HashSet<>();
+        Collection<String> setString = new HashSet<>();
+
+        for(int i=0;i<month.length;i++){
+            Month smonth = new Month(month[i]);
+            setint.add(smonth);
+            setString.add(strmonth[i]);
+            map.put(strmonth[i],month[i]);
+        }
+
+        //使用foreach 遍历map
+        System.out.println("get all key and value");
+        map.forEach((k,v)->{System.out.println(k + " " + v);});
+
+
+        /**
+         *  collection中 没有基本类型
+         *  int 类型需要封箱成 Integer类型
+         *  containsValue( int ) 返回 为false,
+         *  containsValue( Integer ) 返回 为false,
+         */
+        System.out.println("HashMap.containsKey and containsValue ");
+        System.out.println(map.containsKey("Jan"));   //key-value
+        System.out.println(map.containsKey("31"));
+        System.out.println(map.containsValue("Jan"));
+        System.out.println(map.containsValue(31));
+        System.out.println(map.containsValue(new Integer(31)));
+
+        System.out.println("get value by map.key");
+        Set<String> setmonth = map.keySet();  //获取key 集合
+        for(String key :setmonth)
+            System.out.println( key + " " + map.get(key));  //通过key 得到value 值
+
+
+        //获取days集合的迭代器
+        //因为通过hashcode的值 进行存储，所以取出数据时，为hashcode的存储顺序
+        System.out.println("use Iterator get all months(int) and its hashcode");
+
+        Iterator<Month> iterator = setint.iterator();
+        while(iterator.hasNext()){//判断是否有下一个元素
+            Month next = iterator.next();//取出该元素
+            System.out.println(next.days+" hashcode " + next.hashCode() );
+        }
+
+        System.out.println("use Iterator get all months(string) and its hashcode");
+
+        Iterator<String> iteratorstring = setString.iterator();
+        while(iteratorstring.hasNext()){
+            String stringnext = iteratorstring.next();
+            System.out.println(stringnext + " hashcode " + stringnext.hashCode());
+        }
+
+    }
+
+}
+
+class Month{
+    int days;
+    Month(int days){
+        this.days = days;
+    }
+}
+
+
+
---
+++ src/CollectionLearning/JavaEquals.java
@@ -0,0 +1,62 @@
+package CollectionLearning;
+
+import java.util.HashMap;
+
+/**
+ * 重写equals时必须重写 hashcode()
+ * hashCode 方法的常规协定，
+ * 该协定声明相等对象必须具有相等的哈希码
+ *
+ * 如果两个对象相等，则hashcode一定也是相同的
+ * 两个对象相等,对两个对象分别调用equals方法都返回true
+ * 两个对象有相同的hashcode值，它们也不一定是相等的
+ * 因此，equals 方法被覆盖过，则 hashCode 方法也必须被覆盖
+ *
+ * hashCode() 的默认行为是对堆上的对象产生独特值。如果没有重写 hashCode()，则该 class 的两个对象无论如何都不会相等（即使这两个对象指向相同的数据）
+ */
+class People{
+    private String name;
+    private int age;
+
+    public People(String name,int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj)
+            return true;
+   //     if (obj instanceof People) {  //不能使用instance 关键字，因为它不能判断是否为该类的子类
+        if(obj != null && obj.getClass() == this.getClass()){   //使用getClass进行类型判断
+            People obj1 = (People) obj;
+            if (obj1.name == null || name == null) {
+                return false;
+            }
+            return this.name.equals(obj1.name) && this.age == obj1.age;
+        }
+        return  false;
+    }
+
+    @Override
+    public int hashCode(){
+
+        return name.hashCode()*37+age;
+    }
+}
+
+public class JavaEquals {
+
+    public static void main(String[] args) {
+
+        People p1 = new People("Jack", 12);
+        System.out.println(p1.hashCode());
+        System.out.println((new People("Jack",12)).hashCode());
+        HashMap<People, Integer> hashMap = new HashMap<People, Integer>();
+        hashMap.put(p1, 1);
+        System.out.println(p1.equals(new People(null,12)));
+        System.out.println((new People("Jack",12)).equals(p1));
+        System.out.println(hashMap.get(new People("Jack", 12)));
+    }
+}
